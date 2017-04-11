901fec759697e5a524437dc6b965ed6421ac65ab
Guoliang Wang <guwang@mac-guwang.local>
2017-04-11 09:51:11 -0400

    Initial Commit

---
+++ TwitterClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterClient
+//
+//  Created by Wang, Guoliang (Tony) on 4/11/17.
+//  Copyright © 2017 com.rcholic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
