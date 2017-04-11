c44b566264b5aa1bb3508143499b39b41c4a64cb
Wang <guwang@mac-guwang.local>
2017-04-11 10:38:31 -0400

    token works

--- TwitterClient/ViewController.swift
+++ TwitterClient/ViewController.swift
@@ -7,13 +7,22 @@
 //
 
 import UIKit
+import BDBOAuth1Manager
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        loginTwitter()
+    }
+    
+    private func loginTwitter() {
+        TwitterClient.shared.requestSerializer.removeAccessToken()
+        TwitterClient.shared.fetchRequestToken(withPath: "oauth/request_token", method: "GET", callbackURL: URL(string: "http://codepath.com/")!, scope: "scopetest", success: { (requestToken: BDBOAuth1Credential?) in
+            print("token: \(requestToken)")
+        }) { (error: Error?) in
+            print("error: \(error)")
+        }
     }
 }
 
